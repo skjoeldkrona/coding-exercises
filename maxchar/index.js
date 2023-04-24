9dd9eb09ffb26b7fdf36cd613a98d50a25688f7c
skjoeldkrona <skjoeldkrona@gmail.com>
2023-04-24 18:20:29 +0200

    task: MaxChar solution completed

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+  let count = 0;
+  let maxChar = '';
+
+  // for loop for strings and arrays
+  for (let char of str) {
+    chars[char] = chars[char] + 1 || 1;
+  }
+
+  // for loop for objects
+  for (let key in chars) {  
+    if (chars[key] > count) {
+      count = chars[key];
+      maxChar = key;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
